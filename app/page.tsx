579afefc143793567d4bb9da175e9cd890578823
Mohamed Rizwan <mynameisrizwan35@gmail.com>
2024-08-15 21:23:21 +0530

    Emptied the main page /

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 "use client";
-import {Button} from "@/components/ui/button"
-import {LoginButton} from "@/components/auth/login-button"
 import getFormData from "@/actions/getFormData";
 
 export default function Home() {
@@ -11,27 +9,6 @@ export default function Home() {
   return (
     <>
     
-    <main className="flex h-full flex-col items-center justify-center bg-sky-500 text-center">
-      <div className="space-y-6">
-        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
-          Auth
-        </h1>
-        <p className="text-white text-lg">
-          A simple authentication service
-        </p>
-    <LoginButton>
-    <Button>
-          Sign-in
-        </Button>
-      
-      </LoginButton>    
-
-        
-      </div>
-      <button onClick={()=>{sendFormData()}}>
-      form call
-    </button>
-    </main>
     </>
   );
 }
